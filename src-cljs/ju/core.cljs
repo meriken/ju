8bcde0ed4a921874a918a88fab1ba7ee1622e24e
meriken2ch <meriken.2ch@gmail.com>
2016-03-02 22:32:21 -0800

    Bug  fix

--- src-cljs/ju/core.cljs
+++ src-cljs/ju/core.cljs
@@ -1027,7 +1027,7 @@
 
 (defn process-links
   [s]
-  (let [match (re-find #"^(.*?)([htps]+://(www\.)?[-a-zA-Z0-9@:%._\+~#=']{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=']*))(.*)$" s)
+  (let [match (re-find #"^(.*?)([htps]+://((www\.)?[-a-zA-Z0-9@:%._\+~#=']{2,256}\.[a-z]{2,6}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\b([-a-zA-Z0-9@:%_\+.~#?&//=']*))(.*)$" s)
         url (if match (clojure.string/replace (nth match 2) #"^[htp]*[htp]*s://" "https://"))
         url (if match (clojure.string/replace url #"^[htp]+://" "http://"))
         suffix (if match (get (re-find #"\.([a-zA-Z]+)$" (nth match 2)) 1 nil))]
